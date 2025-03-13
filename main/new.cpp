91fb66d06174755c64c86d6ac1c4209af92c7a23
LazySloth <116935885+User-LazySloth@users.noreply.github.com>
2025-03-13 16:03:41 +0530

    Created new.cpp

---
+++ main/new.cpp
@@ -0,0 +1,10 @@
+//File: new.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello World!\n";
+  cout << "This is a new file\n";
+  cout << "I have successfully built and run this\n";
+  return 0;
+}
